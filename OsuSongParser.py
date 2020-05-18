dc2481d5b7091da8f206ff6b9b0953842d170822
cira0100 <40918547+cira0100@users.noreply.github.com>
2020-05-18 02:22:42 +0200

    Add files via upload

---
+++ OsuSongParser.py
@@ -0,0 +1,26 @@
+import re
+import os
+import shutil
+regexComp=re.compile(r'^[0-9]*')
+regexMp3=re.compile(r'\.mp3$')
+cwd = os.getcwd()
+sum=0
+s=input("type path to Osu Songs \n")
+for filename in os.listdir(s):
+    sum=sum+1
+for filename in os.listdir(s):
+    test=regexComp.search(filename)
+    songName=filename[len(test.group(0))+1:]
+    path=os.path.join(s,filename)
+    sum=sum-1
+    print("Files left ........ "+str(sum))
+    for name in os.listdir(path):
+        if(regexMp3.search(name)):
+            shutil.copy(os.path.join(path,name),cwd)
+            os.rename(os.path.join(cwd,name),os.path.join(cwd,songName))
+            break
+            
+print("Done")
+
+
+
